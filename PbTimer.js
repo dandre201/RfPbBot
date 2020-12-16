7f59b3333b5d82da8c386c7db39f63d7cb55f565
dandre201 <darrenandrew12@gmail.com>
2020-12-16 23:16:40 +0000

    moved logic to functions

--- PbTimer.js
+++ PbTimer.js
@@ -17,55 +17,37 @@ client.on('ready', () => {
 
  	var input = message.content.toUpperCase();
 
- 	 if(input === "?MAIN")	{
+ 	if(input === "?MAIN")	{
+      message.reply(getDraco());
 
-     //draco pop on 15/11/2020  -
-     var lastDraco= 1605438480000;
-     var dracoRePop= 115200000;  // 32 hours in ms
-     var dracoName= "Draco";
-     var dracoPopMsg = getNextPop( dracoName,lastDraco,dracoRePop);
-     message.reply(dracoPopMsg);
+      message.reply(get3d());
 
-  //last 3d =   14/11/2020
-  var last3D = 1605343200000;
-  var threeDRepop = 230400000; // 64 hours in ms
-  var threeDName= "3D";
-  var threeDPopMsg = getNextPop( threeDName,last3D,threeDRepop);
-  message.reply(threeDPopMsg);
+      message.reply(getGil());
 
-  //gil  every 96.5    - 14/11 14:40
-  var lastGil = 1605364800000;
-  var gilRepop = 	347400000; // 96.5 hours in ms
-  var gilName= "Gil";
-  var gilPopMsg = getNextPop( gilName,lastGil,gilRepop);
-  message.reply(gilPopMsg);
+      message.reply(getOcl());
 
-
-  //ocl  96h  345600000
-  var lastOcl = 1605323280000;
-  var oclRepop = 	345600000; // 96 hours in ms
-  var oclName= "RingLeader";
-  var oclPopMsg = getNextPop( oclName,lastOcl,oclRepop);
-  message.reply(oclPopMsg);
-
-  //Nova
-  var lastNova = 1608050700000;
-  var novaRepop = 14400000; //4 hours  to ms
-  var novaName= "Nova";
-  var novaPopMsg = getNextPop( novaName,lastNova,novaRepop);
-  message.reply(novaPopMsg);
-
+      message.reply(getNova());
 
     }
 
     else if (input ==="?DRACO"){
-      //draco pop on 15/11/2020  -
-      var lastDraco= 1605438480000;
-      var dracoRePop= 115200000;  // 32 hours in ms
-      var dracoName= "Draco";
-      var dracoPopMsg = getNextPop( dracoName,lastDraco,dracoRePop);
-      message.reply(dracoPopMsg);
+      message.reply(getDraco());
+    }
 
+    else if (input ==="?NOVA"){
+      message.reply(getNova());
+    }
+
+    else if (input ==="?RINGLEADER"){
+      message.reply(getOcl());
+    }
+
+    else if (input ==="?GIL"){
+      message.reply(getGil());
+    }
+
+    else if (input ==="?3D"){
+      message.reply(get3d());
     }
 
     else if (input === "?ELAN"){
@@ -118,48 +100,32 @@ client.on('ready', () => {
     else if (input === "?OTHER"){
       // dims, ether cali
 
+      message.reply(getEq());
 
-      //EQ .. 22:02
-      var lastEq = 1605564150000;
-      var eqRepop = 14430000; //4 hours 30 secs to ms
-      var eqName= "EQ";
-      var eqPopMsg = getNextPop( eqName,lastEq,eqRepop);
-      message.reply(eqPopMsg);
+      message.reply(getPig());
 
-      //gold pig 10:43 ?
-      var lastEq = 1605523380000;
-      var eqRepop = 86400000; //24 hours to ms
-      var eqName= "Gold Pig";
-      var eqPopMsg = getNextPop( eqName,lastEq,eqRepop);
-      message.reply(eqPopMsg);
-
-      //k1 - 20:44
-      var lastk = 1605559440000;
-      var kRepop = 28800000; // 8 hours to ms
-      var kName= "K1/K2";
-      var kPopMsg = getNextPop( kName,lastk,kRepop);
-      message.reply(kPopMsg);
+      message.reply(getK1());
 
     }
 
-  else if (input === "?MEH"){
-    //herodian 24h  86400000
-    var lastHero = 1605403080000;
-    var heroRepop = 	86400000; // 24 hours in ms
-    var heroName= "herodian";
-    var heroPopMsg = getNextPop( heroName,lastHero,heroRepop);
-    message.reply(heroPopMsg);
-    message.reply("Herodian Pb pop time changes after maintenance, this tiem is the earlies and it can be anytime +2 hours");
+
+    else if (input === "?EQ"){
+      message.reply(getEq());
     }
 
-    else if (input === "?TEST"){
-      message.reply(getPbTime());
+    else if (input === "?PIG"){
+      message.reply(getPig());
+    }
+
+    else if (input === "?HERODIAN"){
+      message.reply(getHerodian());
+      message.reply("Herodian Pb pop time changes after maintenance, this tiem is the earlies and it can be anytime +2 hours");
       }
 
     else if (input === "?HELP"){
 
-      var result = ("Use the follow command to see PB's:  ?Main,  ?Elan, ?HQ, ?Other ?Meh"  );
-      message.reply(result);
+      message.reply("Use the follow command to see PB's:  ?Main,  ?Elan, ?HQ, ?Other"  );;
+      message.reply("You can also type the PB name: ?Draco, ?Ringleader , ?Nova , etc"  );;
     }
  });
 
@@ -191,7 +157,79 @@ var d = new Date();
     }
 }
 
-function getPbTime(){
+function getOcl(){
+  //ocl  96h  345600000
+  var lastOcl = 1605323280000;
+  var oclRepop = 	345600000; // 96 hours in ms
+  var oclName= "RingLeader";
+  var oclPopMsg = getNextPop( oclName,lastOcl,oclRepop);
+  return oclPopMsg;
+}
+
+function getNova(){
+  //Nova
+  var lastNova = 1608050700000;
+  var novaRepop = 14400000; //4 hours  to ms
+  var novaName= "Nova";
+  var novaPopMsg = getNextPop( novaName,lastNova,novaRepop);
+  return novaPopMsg;
+}
+
+function getGil(){
+  //gil  every 96.5    - 14/11 14:40
+  var lastGil = 1605364800000;
+  var gilRepop = 	347400000; // 96.5 hours in ms
+  var gilName= "Gil";
+  var gilPopMsg = getNextPop( gilName,lastGil,gilRepop);
+  return gilPopMsg;
+}
+
+function getDraco(){
+  //draco pop on 15/11/2020  -
+  var lastDraco= 1605438480000;
+  var dracoRePop= 115200000;  // 32 hours in ms
+  var dracoName= "Draco";
+  var dracoPopMsg = getNextPop( dracoName,lastDraco,dracoRePop);
+  return dracoPopMsg;
+}
+
+function get3d(){
+  //last 3d =   14/11/2020
+  var last3D = 1605343200000;
+  var threeDRepop = 230400000; // 64 hours in ms
+  var threeDName= "3D";
+  var threeDPopMsg = getNextPop( threeDName,last3D,threeDRepop);
+  return threeDPopMsg;
+}
+
+function getEq(){
+  //EQ .. 22:02
+  var lastEq = 1605564150000;
+  var eqRepop = 14430000; //4 hours 30 secs to ms
+  var eqName= "EQ";
+  var eqPopMsg = getNextPop( eqName,lastEq,eqRepop);
+  return eqPopMsg;
+}
+
+function getPig(){
+  //gold pig 10:43 ?
+  var lastPig = 1605523380000;
+  var pigRepop = 86400000; //24 hours to ms
+  var pigName= "Gold Pig";
+  var pigPopMsg = getNextPop( pigName,lastPig,pigRepop);
+  return pigPopMsg;
+}
+
+function getK1(){
+  //k1 - 20:44
+  var lastk = 1605559440000;
+  var kRepop = 28800000; // 8 hours to ms
+  var kName= "K1/K2";
+  var kPopMsg = getNextPop( kName,lastk,kRepop);
+  return kPopMsg
+}
+
+function getHerodian(){
   var lastHero = 1605403080000;
   var heroRepop = 	86400000; // 24 hours in ms
   var heroName= "herodian";
